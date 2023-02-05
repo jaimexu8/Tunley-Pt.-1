8a2842f0ad9036601cd4c80cc4342ac930ac37c1
Jaime Xu <jaimexu8@gmail.com>
2023-02-05 17:15:03 -0500

    initial commit

---
+++ lab-tunley/TrackCell.swift
@@ -0,0 +1,36 @@
+//
+//  TrackCell.swift
+//  lab-tunley
+//
+//  Created by Jaime Xu on 2/5/23.
+//
+
+import UIKit
+import Nuke
+
+class TrackCell: UITableViewCell {
+
+    @IBOutlet weak var trackImageView: UIImageView!
+    @IBOutlet weak var trackNameLabel: UILabel!
+    @IBOutlet weak var artistNameLabel: UILabel!
+    
+    /// Configures the cell's UI for the given track.
+    func configure(with track: Track) {
+        trackNameLabel.text = track.trackName
+        artistNameLabel.text = track.artistName
+
+        // Load image async via Nuke library image loading helper method
+        Nuke.loadImage(with: track.artworkUrl100, into: trackImageView)
+    }
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+}
